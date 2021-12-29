388e8fad77744fbc66b7f70c9e0876116a4a799c
srewai <rauniyar1sonu@gmail.com>
2021-12-29 20:29:22 +0100

    modified weather logic

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int main(){
     
 
     do {
-    cout << "I am intent Recognizer, Please type something & I will try to guess your intention!" << endl;
+    cout << "I am intent Recognizer, Please type something & I will try to guess your intention!\nTo EXIT type 'ciao'" << endl;
     //clear input 
     getinput.clear();
     getline(cin,getinput);
@@ -32,37 +32,69 @@ int main(){
     */
 
     //LOGIC
-     if((regex_search(getinput, regex("atmospheric conditions")) || regex_search(getinput, regex("sunny")) || regex_search(getinput, regex("cloudy")) 
-        || regex_search(getinput, regex("rainy")) || regex_search(getinput, regex("atmosphere")) || regex_search(getinput, regex("climate")) || regex_search(getinput, regex("tornadoes"))
-        || regex_search(getinput, regex("airy")) || regex_search(getinput, regex("cold")) || regex_search(getinput, regex("hot")) || regex_search(getinput, regex("chill"))
-        || regex_search(getinput, regex("chilling")) || regex_search(getinput, regex("warm")) || regex_search(getinput, regex("tornado")) || regex_search(getinput, regex("temperature"))
-        || regex_search(getinput, regex("humidity")) || regex_search(getinput, regex("cloud")) || regex_search(getinput, regex("cloudy")) || regex_search(getinput, regex("wind"))
-        || regex_search(getinput, regex("windy")) || regex_search(getinput, regex("meteor")) || regex_search(getinput, regex("air")) || regex_search(getinput, regex("weather"))
-        || regex_search(getinput, regex("nice day")))
-        && regex_search(getinput, regex("in")))
-
+     if (
+         (regex_search(getinput, regex("atmospheric")) || 
+            regex_search(getinput, regex("sunny")) || 
+            regex_search(getinput, regex("cloudy")) || 
+            regex_search(getinput, regex("rainy")) || 
+            regex_search(getinput, regex("atmosphere")) || 
+            regex_search(getinput, regex("climate")) || 
+            regex_search(getinput, regex("tornadoes")) || 
+            regex_search(getinput, regex("airy")) || 
+            regex_search(getinput, regex("cold")) || 
+            regex_search(getinput, regex("hot")) || 
+            regex_search(getinput, regex("chill")) || 
+            regex_search(getinput, regex("chilling")) || 
+            regex_search(getinput, regex("warm")) || 
+            regex_search(getinput, regex("tornado")) || 
+            regex_search(getinput, regex("temperature")) || 
+            regex_search(getinput, regex("humidity")) || 
+            regex_search(getinput, regex("cloud")) || 
+            regex_search(getinput, regex("cloudy")) || 
+            regex_search(getinput, regex("wind")) || 
+            regex_search(getinput, regex("windy")) || 
+            regex_search(getinput, regex("meteor")) || 
+            regex_search(getinput, regex("air")) || 
+            regex_search(getinput, regex("weather")) || 
+            regex_search(getinput, regex("nice day"))) &&
+         regex_search(getinput, regex("in")))
+     
+    {
         cout << "Intent: Get Weather City" << endl;
+     }
 
-    else if (regex_search(getinput, regex("atmospheric conditions")) || regex_search(getinput, regex("sunny")) || regex_search(getinput, regex("cloudy")) 
+    else if 
+    (regex_search(getinput, regex("atmospheric conditions")) || regex_search(getinput, regex("sunny")) || regex_search(getinput, regex("cloudy")) 
         || regex_search(getinput, regex("rainy")) || regex_search(getinput, regex("atmosphere")) || regex_search(getinput, regex("climate")) || regex_search(getinput, regex("tornadoes"))
         || regex_search(getinput, regex("airy")) || regex_search(getinput, regex("cold")) || regex_search(getinput, regex("hot")) || regex_search(getinput, regex("chill"))
         || regex_search(getinput, regex("chilling")) || regex_search(getinput, regex("warm")) || regex_search(getinput, regex("tornado")) || regex_search(getinput, regex("temperature"))
         || regex_search(getinput, regex("humidity")) || regex_search(getinput, regex("cloud")) || regex_search(getinput, regex("cloudy")) || regex_search(getinput, regex("wind"))
-        || regex_search(getinput, regex("windy")) || regex_search(getinput, regex("meteor")) || regex_search(getinput, regex("air")) || regex_search(getinput, regex("weather")))
+        || regex_search(getinput, regex("windy")) || regex_search(getinput, regex("meteor")) || regex_search(getinput, regex("air")) 
+        || regex_search(getinput, regex("weather")))
         
-        
+    {    
         cout << "Intent: Get Weather" << endl;
+    }
 
-    else if (regex_search(getinput, regex("fact")) || regex_search(getinput, regex("something interesting")) || regex_search(getinput, regex("something interesting"))
+    else if 
+    (regex_search(getinput, regex("fact")) || regex_search(getinput, regex("something interesting")) || regex_search(getinput, regex("something interesting"))
         || regex_search(getinput, regex("truth")) || regex_search(getinput, regex("actuality")) || regex_search(getinput, regex("reality")) || regex_search(getinput, regex("verity")))
-
+    {
         cout << "Intent: Get Fact" << endl;
+    }
+        
 
     else if (regex_search(getinput, regex("ciao")))
+    {
         cout << "************************Auf Wierdershen***************************\n" << endl;
+    }
+        
 
     else
+    {
         cout << "\nI am still learning, Sorry I am not able to find your intention, \nPlease try one more time!\n" << endl;
+    }
+        
 
     } while (getinput != "ciao");
 
