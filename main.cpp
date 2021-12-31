d404d44eecb1a84f373ccaef5c1eb01ab45b7ab6
srewai <rauniyar1sonu@gmail.com>
2021-12-31 17:20:13 +0100

    simple restrctured and included CTest

--- main.cpp
+++ main.cpp
@@ -1,103 +1,87 @@
-#include "main.h"
+// Determine intent of a sentence.
+// Input - quoted string as argv[1].
+// Output - guessed intent on stdout.
+//
+// My ideal solution would be to train an ML model
+// but since the scope of the task at hand is constrained,
+// I am using a simple deterministic approach.
 
-using namespace std;
 
-int main(){
+#include <cassert>
+#include <iostream>
+#include <numeric>
+#include <regex>
+#include <string>
 
-    // decalre input variable
-    string getinput;
 
-    // Message display
-    cout << endl;
-    cout << "This is simple intent recognizer that tries to capture semantic variations of the below three utternaces" << endl;
-    cout << "****************************************************************************" << endl;
-    cout << "1.What is the weather like today? => Prints (Intent: Get Weather)" << endl;
-    cout << "2.What is the weather like in Paris today? => Prints (Intent: Get Weather City)" << endl;
-    cout << "3.Tell me an interesting fact. => Prints (Intent: Get Fact)" << endl;
-    cout << "****************************************************************************" << endl;
-    
+std::string guess_intent( const std::string & input )
+{
+    const auto weather_city{ std::regex("atmospheric|sunny|cloudy|rainy|"
+                                        "atmosphere|climate|tornadoes|airy|"
+                                        "cold|hot|chill|chilling|warm|tornado|"
+                                        "temperature|humidity|cloud|cloudy|"
+                                        "wind|windy|meteor|air|weather|nice day") };
+    const auto weather_general{ std::regex("atmospheric conditions|sunny|"
+                                           "cloudy|rainy|atmosphere|climate|"
+                                           "tornadoes|airy|cold|hot|chill"
+                                           "chilling|warm|tornado|temperature|"
+                                           "humidity|cloud|cloudy|wind|windy|"
+                                           "meteor|air|weather") };
+    const auto facts{ std::regex("fact|something interesting|truth|actuality|" 
+                                                            "reality|verity") };
 
-    do {
-    cout << "I am intent Recognizer, Please type something & I will try to guess your intention!\nTo EXIT type 'ciao'" << endl;
-    //clear input 
-    getinput.clear();
-    getline(cin,getinput);
-
-    // for unifrmity let's change the input into lower case
-    transform(getinput.begin(),getinput.end(),getinput.begin(), ::tolower);
-
-    /*
-    My ideal solution to this problem would be train an ML model but since the ask is simple command line tool with time constraint, I am using simple if-else with do-while loop
-
-    */
-
-    //LOGIC
-     if (
-         (regex_search(getinput, regex("atmospheric")) || 
-            regex_search(getinput, regex("sunny")) || 
-            regex_search(getinput, regex("cloudy")) || 
-            regex_search(getinput, regex("rainy")) || 
-            regex_search(getinput, regex("atmosphere")) || 
-            regex_search(getinput, regex("climate")) || 
-            regex_search(getinput, regex("tornadoes")) || 
-            regex_search(getinput, regex("airy")) || 
-            regex_search(getinput, regex("cold")) || 
-            regex_search(getinput, regex("hot")) || 
-            regex_search(getinput, regex("chill")) || 
-            regex_search(getinput, regex("chilling")) || 
-            regex_search(getinput, regex("warm")) || 
-            regex_search(getinput, regex("tornado")) || 
-            regex_search(getinput, regex("temperature")) || 
-            regex_search(getinput, regex("humidity")) || 
-            regex_search(getinput, regex("cloud")) || 
-            regex_search(getinput, regex("cloudy")) || 
-            regex_search(getinput, regex("wind")) || 
-            regex_search(getinput, regex("windy")) || 
-            regex_search(getinput, regex("meteor")) || 
-            regex_search(getinput, regex("air")) || 
-            regex_search(getinput, regex("weather")) || 
-            regex_search(getinput, regex("nice day"))) &&
-         regex_search(getinput, regex("in")))
-     
+    if( std::regex_search( input, weather_city ) &&
+        std::regex_search( input, std::regex("in") ))
     {
-        cout << "Intent: Get Weather City" << endl;
-     }
-
-    else if 
-    (regex_search(getinput, regex("atmospheric conditions")) || regex_search(getinput, regex("sunny")) || regex_search(getinput, regex("cloudy")) 
-        || regex_search(getinput, regex("rainy")) || regex_search(getinput, regex("atmosphere")) || regex_search(getinput, regex("climate")) || regex_search(getinput, regex("tornadoes"))
-        || regex_search(getinput, regex("airy")) || regex_search(getinput, regex("cold")) || regex_search(getinput, regex("hot")) || regex_search(getinput, regex("chill"))
-        || regex_search(getinput, regex("chilling")) || regex_search(getinput, regex("warm")) || regex_search(getinput, regex("tornado")) || regex_search(getinput, regex("temperature"))
-        || regex_search(getinput, regex("humidity")) || regex_search(getinput, regex("cloud")) || regex_search(getinput, regex("cloudy")) || regex_search(getinput, regex("wind"))
-        || regex_search(getinput, regex("windy")) || regex_search(getinput, regex("meteor")) || regex_search(getinput, regex("air")) 
-        || regex_search(getinput, regex("weather")))
-        
-    {    
-        cout << "Intent: Get Weather" << endl;
+        return { "Intent: Get Weather City" };
     }
-
-    else if 
-    (regex_search(getinput, regex("fact")) || regex_search(getinput, regex("something interesting")) || regex_search(getinput, regex("something interesting"))
-        || regex_search(getinput, regex("truth")) || regex_search(getinput, regex("actuality")) || regex_search(getinput, regex("reality")) || regex_search(getinput, regex("verity")))
+    else if( std::regex_search( input, weather_general) )
     {
-        cout << "Intent: Get Fact" << endl;
+        return { "Intent: Get Weather" };
     }
-        
-
-    else if (regex_search(getinput, regex("ciao")))
+    else if( std::regex_search( input, facts ) )
     {
-        cout << "************************Auf Wierdershen***************************\n" << endl;
+        return { "Intent: Get Fact" };
     }
-        
-
     else
     {
-        cout << "\nI am still learning, Sorry I am not able to find your intention, \nPlease try one more time!\n" << endl;
+        return { "Intent unknown, let's talk about the weather." };
     }
-        
+}
 
-    } while (getinput != "ciao");
 
-    
-return 0;
+void test()
+{
+    assert( guess_intent( "What is the weather like today?").compare(
+                          "Intent: Get Weather" ) == 0 );
+    assert( guess_intent( "What is the weather like in Paris today?").compare(
+                          "Intent: Get Weather City" ) == 0 );
+    assert( guess_intent( "Tell me an interesting fact.").compare(
+                          "Intent: Get Fact" ) == 0 );
+}
+
+
+int main( int argc, char ** argv)
+{
+    if( argc != 2 )
+    {
+        std::cerr << "Please call with a quoted sentence or phrase as parameter.";
+        return EXIT_FAILURE;
+    }
+
+    // Let's work in lowercase only.
+    const std::string input_raw{ argv[1] };
+    const auto input{ std::accumulate( input_raw.cbegin()
+                                    , input_raw.cend()
+                                    , std::string{} ) };
+
+    if( std::string{"test"}.compare( input ) == 0 )
+    {
+        test();
+        std::cout << "Tests passed.\n";
+        return EXIT_SUCCESS;
+    }
+
+    std::cout << guess_intent( input ) << std::endl;
+    return EXIT_SUCCESS;
 }
